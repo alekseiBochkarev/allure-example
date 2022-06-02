275918e3eb3874577f566c5243757f29434ac396
Artem Eroshenko <eroshenkoam@qameta.io>
2022-06-02 14:03:13 +0300

    add test 1534

--- src/test/java/io/qameta/allure/AuthTest.java
+++ src/test/java/io/qameta/allure/AuthTest.java
@@ -31,4 +31,23 @@ public class AuthTest {
         step("Разлогиниваемся");
     }
 
+    @Test
+    @AllureId("1534")
+    @DisplayName("Авторизация через Facebook")
+    public void testFacebookAuth() {
+        step("Открываем главную страницу");
+        step("Выбираем способ авторизации через Facebook");
+        step("Авторизуемся как пользователь `Mr. Random`", () -> {
+            step("Вводим логин `random-user@gmail.com`");
+            step("Вводим пароль `random-pass`");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Проверяем что авторизовались успешно", () -> {
+            step("Логин `random-user`");
+            step("Имя `Mr. Random`");
+            step("Email `random-user@gmail.com`");
+        });
+        step("Разлогиниваемся");
+    }
+
 }
